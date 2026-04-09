602c80822483dc7aab7b7cf8c9aca447d95fe616
Youssif Hamdy <youssifhamdy381@gmail.com>
2026-04-09 18:17:40 +0200

    fix swagger output path

--- my-app/server.js
+++ my-app/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output.json");
+const swaggerFile = require("./config/swagger-output.json");
 const menuRoutes = require("./routes/menuRoutes");
 
 const app = express();
